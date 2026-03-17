aa574e47c718219b4bdc5ee4b61280660b73124f
Rukhaiya59 <rukhaiyamahbub@gmail.com>
2026-03-17 17:18:12 +0530

    disable mongo temp

--- src/main/kotlin/org/example/database/mongo_factory.kt
+++ src/main/kotlin/org/example/database/mongo_factory.kt
@@ -1,14 +1,3 @@
-package org.example.database
-
-import com.mongodb.kotlin.client.coroutine.MongoClient
-import com.mongodb.kotlin.client.coroutine.MongoDatabase
-
 object MongoFactory {
-
-    private val client = MongoClient.create(
-        "mongodb+srv://hotel_user:1234@cluster0.abcd.mongodb.net/hotel_db?retryWrites=true&w=majority"
-    )
-
-    val database: MongoDatabase =
-        client.getDatabase("hotel_db")
+    val database = null
 }
